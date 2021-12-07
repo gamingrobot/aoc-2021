72579ac85bf2efede9e64d3c9917e267ef10803f
Morgan Creekmore <morgan@creekmore.email>
2021-12-06 23:39:20 -0600

    Day 2

--- src/day1.rs
+++ src/day1.rs
@@ -7,23 +7,28 @@ fn gen(input: &str) -> Result<Vec<u32>, ParseIntError> {
 
 #[aoc(day1, part1, first)]
 pub fn part1(input: &[u32]) -> u32 {
-   let mut prev: u32 = *input.first().unwrap();
-   let mut count = 0;
-   for i in input {
-      if i > &prev {
-         count += 1;
-      }
-      prev = *i;
-   }
-   count
+    let mut prev: u32 = *input.first().unwrap();
+    let mut count = 0;
+    for i in input {
+        if i > &prev {
+            count += 1;
+        }
+        prev = *i;
+    }
+    count
 }
 
 #[aoc(day1, part1, iterator)]
 pub fn part1_iterator(input: &[u32]) -> usize {
-   input.windows(2).filter(|&w| w[1] > w[0]).count()
+    input.windows(2).filter(|&w| w[1] > w[0]).count()
 }
 
 #[aoc(day1, part2)]
 pub fn part2(input: &[u32]) -> usize {
-   input.windows(3).collect::<Vec<_>>().windows(2).filter(|&w| (w[1][0] + w[1][1] + w[1][2])  > (w[0][0] + w[0][1] + w[0][2])).count()
+    input
+        .windows(3)
+        .collect::<Vec<_>>()
+        .windows(2)
+        .filter(|&w| (w[1][0] + w[1][1] + w[1][2]) > (w[0][0] + w[0][1] + w[0][2]))
+        .count()
 }
---
+++ src/day2.rs
@@ -0,0 +1,82 @@
+use std::{num::ParseIntError, str::FromStr};
+
+#[aoc_generator(day2)]
+fn gen(input: &str) -> Result<Vec<Command>, ParseIntError> {
+    let mut output = Vec::new();
+    for line in input.lines() {
+        let mut split = line.split(' ');
+        let direction = split.next().unwrap().parse().unwrap();
+        let unit = split.next().unwrap().parse().unwrap();
+        output.push(Command::new(direction, unit))
+    }
+    Ok(output)
+}
+
+#[aoc(day2, part1)]
+pub fn part1(input: &[Command]) -> u32 {
+    let mut depth: u32 = 0;
+    let mut horizontal: u32 = 0;
+    for command in input {
+        match command.direction {
+            Direction::Forward => horizontal += command.unit,
+            Direction::Down => depth += command.unit,
+            Direction::Up => depth -= command.unit,
+        }
+    }
+    println!("{:?}", depth);
+    println!("{:?}", horizontal);
+    horizontal * depth
+}
+
+#[aoc(day2, part2)]
+pub fn part2(input: &[Command]) -> u32 {
+    let mut depth: u32 = 0;
+    let mut horizontal: u32 = 0;
+    let mut aim: u32 = 0;
+    for command in input {
+        match command.direction {
+            Direction::Forward => {
+                horizontal += command.unit;
+                depth += aim * command.unit;
+            },
+            Direction::Down => aim += command.unit,
+            Direction::Up => aim -= command.unit,
+        }
+    }
+    println!("{:?}", depth);
+    println!("{:?}", horizontal);
+    println!("{:?}", aim);
+    horizontal * depth
+}
+
+#[derive(Debug)]
+pub struct Command {
+    direction: Direction,
+    unit: u32,
+}
+
+impl Command {
+    fn new(direction: Direction, unit: u32) -> Command {
+        Command { direction, unit }
+    }
+}
+
+#[derive(Debug)]
+enum Direction {
+    Forward,
+    Down,
+    Up,
+}
+
+impl FromStr for Direction {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Direction, ()> {
+        match s {
+            "forward" => Ok(Direction::Forward),
+            "down" => Ok(Direction::Down),
+            "up" => Ok(Direction::Up),
+            _ => Err(()),
+        }
+    }
+}
